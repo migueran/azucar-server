5eac9a613d6e926ab80e0b933f21bb25760e33fe
mike! <mrando.via@gmail.com>
2026-04-06 19:49:14 -0300

    feat: added Encoder

--- src/main/java/com/mrando/azucardj/controller/HomeController.java
+++ src/main/java/com/mrando/azucardj/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.mrando.azucardj.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HomeController {
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   @GetMapping("/")
   public String Home() {
       return "hola";
+  }
+
+  @GetMapping("/bcrypt/{text}")
+  @ResponseBody
+  public String Encoder(@PathVariable("text") String text) {
+    return text + " Encriptado en Bcrypt: " + passwordEncoder.encode(text);
   }
 
 }
--- src/main/java/com/mrando/azucardj/security/DatabaseWebSecurity.java
+++ src/main/java/com/mrando/azucardj/security/DatabaseWebSecurity.java
@@ -4,18 +4,46 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class DatabaseWebSecurity {
 
   @Bean
-  UserDetailsManager users( DataSource dataSource) {
+  UserDetailsManager usersDbManager( DataSource dataSource) {
     JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    users.setUsersByUsernameQuery("SELECT username, password, status FROM users WHERE username = ?");
+    users.setAuthoritiesByUsernameQuery("select u.username, p.profile from UsersProfiles up " + 
+                                        "inner join Users u on u.id = u.id " + 
+                                        "inner join Profiles p on ur.id = p.id " + 
+                                        "where u.username = ?");
     return users;
   }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authz) -> authz
+        .requestMatchers("/**").permitAll()
+        // .requestMatchers("/**").hasAuthority("admin")
+        // .requestMatchers("/**").hasAuthority("writer")
+        // .requestMatchers("/**").hasAuthority("guest")
+        // .requestMatchers("/", "/lists", "/artists").permitAll()
+        .anyRequest().authenticated()
+      );
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
